a5d408b3ab1ef571ac3ca758bd332e615a861566
tetianavinnik <92581380+tetianavinnik@users.noreply.github.com>
2022-03-28 17:02:48 +0300

    Update logistic_system.py

--- logistic_system.py
+++ logistic_system.py
@@ -39,36 +39,3 @@ class LogisticSystem:
 Total price: {2} UAH.'\
                              .format(i.orderId, i.location.city, i.calcPrice()))
         return print('No such order.')
-
-
-# from logistic_system import LogisticSystem
-# from order import Order
-# from ivl import Item, Vehicle, Location
-
-# >>> vehicles = [Vehicle(1), Vehicle(2)]
-
-# >>> logSystem = LogisticSystem(vehicles)
-
-# >>> my_items = [Item('book',110), Item('chupachups',44)]
-
-# >>> my_order = Order(user_name = 'Oleg', city = 'Lviv', postoffice = 53, items = my_items)
-
-# >>> logSystem.placeOrder(my_order)
-
-# >>> logSystem.trackOrder()
-
-# >>> my_items2 = [Item('flowers',11), Item('shoes',153), Item('helicopter',0.33)]
-
-# >>> my_order2 = Order('Andrii', 'Odessa', 3, my_items2)
-
-# >>> logSystem.placeOrder(my_order2)
-
-# >>> logSystem.trackOrder(234976475)
-
-# >>> my_items3 = [Item('coat',61.8), Item('shower',5070), Item('rollers',700)]
-
-# >>> my_order3 = Order('Olesya', 'Kharkiv', 17, my_items3)
-
-# >>> logSystem.placeOrder(my_order3)
-
-# >>> logSystem.trackOrder(485932990)
